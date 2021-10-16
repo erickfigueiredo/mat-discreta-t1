42bcfb1250a92495a68f2595c2cd0b2c93c30ea3
Erick Figueiredo <ericklima816@gmail.com>
2021-10-16 01:04:58 -0300

    UPDATED - Protótipo geração de combinações com permutação

--- 2-OP/OP.cpp
+++ 2-OP/OP.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void toBinary (vector <bool> &c, int num, int cursor = 1) {
-    if(num == 1) {
+void toBinary(vector<bool>& c, int num, int cursor = 1) {
+    if (num == 1) {
         c[c.size() - cursor] = true;
         return;
     }
 
-    if(num % 2 == 0) {
+    if (num % 2 == 0) {
         c[c.size() - cursor] = false;
-    } else {
+    }
+    else {
         c[c.size() - cursor] = true;
     }
-    
-    toBinary(c, num/2, ++cursor);
+
+    toBinary(c, num / 2, ++cursor);
 }
 
 int main() {
@@ -27,15 +29,23 @@ int main() {
 
     int numCombinations = 1 << n;
 
-    for(int i = 0; i < n; i++) nodes[i] = i+1;
+    for (int i = 0; i < n; i++)
+        nodes[i] = i + 1;
 
-    for (int i = 1; i < numCombinations; i++) 
-    {
+    for (int i = 1; i < numCombinations; i++) {
+        vector <int> perm;
         toBinary(combination, i);
 
-        for(int i = 0; i < combination.size(); i++ ) {
-            if(combination[i]) cout << nodes[i];
+        // Aqui exibe a combinação
+        for (int i = 0; i < combination.size(); i++) {
+            if (combination[i])
+                perm.push_back(nodes[i]);
         }
+
+        do {
+            for(int i = 0; i < perm.size(); i++) cout << perm[i] << ' ';
+            cout << '\n';
+        } while(next_permutation(perm.begin(), perm.end()));
         cout << '\n';
     }
 
